f12a675a39384280bc3118a3798e6333daceb1af
gillbertmpadon <202080194@psu.palawan.edu.ph>
2022-09-19 21:01:27 +0800

    added python folder

---
+++ pythonSetup/dailyPython.py
@@ -0,0 +1,34 @@
+#This is a one line comment
+'''
+multiple lines of comment
+'''
+
+"""
+double quotes multiple comments
+"""
+
+print("Gin Toki Sakata")
+
+#the indention is a must
+age = 5
+if age >4:
+    print('the age is ',age)
+    print('not minor')
+else:
+    print('minor')
+
+#python can do casting from differ variable type
+x = 100
+print(x)
+x = 'cast to string'
+print(x)
+
+#casting can be specific with declare type
+y = 10 #this is int 10
+z = str(y) #this will be '10'
+flo = float(y) # this string will be 10.0
+print(y)
+print(z)
+print(flo)
+
+#
